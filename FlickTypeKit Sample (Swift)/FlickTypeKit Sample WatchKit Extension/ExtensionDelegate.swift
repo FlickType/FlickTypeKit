723c04ec6dfcacdc7b334341fc3cf9f7c11006d9
Kosta Eleftheriou <kostas.eleftheriou@gmail.com>
2020-08-23 13:11:34 -0700

    Update ExtensionDelegate.swift

--- FlickTypeKit Sample (Swift)/FlickTypeKit Sample WatchKit Extension/ExtensionDelegate.swift
+++ FlickTypeKit Sample (Swift)/FlickTypeKit Sample WatchKit Extension/ExtensionDelegate.swift
@@ -10,27 +10,15 @@ import WatchKit
 import FlickTypeKit
 
 class ExtensionDelegate: NSObject, WKExtensionDelegate {
-
-    func applicationDidFinishLaunching() {
-        // Perform any final initialization of your application.
-        FlickType.returnURL = URL(string: "https://www.flicktype.com/flicktype")
-    }
-
-    func applicationDidBecomeActive() {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillResignActive() {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, etc.
-    }
-
-}
-
-extension ExtensionDelegate {
+  
+  func applicationDidFinishLaunching() {
+    // This should be your own app url. The recommended format is "https://your.app.domain/flicktype/"
+    FlickType.returnURL = URL(string: "https://www.flicktype.com/flicktype/")
+  }
   
   func handle(_ userActivity: NSUserActivity) {
     if FlickType.handle(userActivity) { return }
+    // Handle other activities here
     print("Received non-FlickType activity \(userActivity)")
   }
   
